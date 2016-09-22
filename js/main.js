814313491825e565f8848d7624c5805f61f4312b
YuriyChaban <shuyterrush@gmail.com>
2016-09-22 08:22:40 +0300

    prepead files for development

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// for main menu
+$(function () {
+    $('.list-menu li a').each(function () {
+        var location = window.location.pathname;
+        var link = this.pathname;
+        var result = location.match(link);
+        if(result && link.length > 0) {
+            $(this).addClass('list-menu-active');
+        }
+    });
+});
+
+// mobile slider
+// var swiper_main = new Swiper('.swiper-container-bilet-more-mobile', {
+//     paginationClickable: true,
+//     loop: true,
+//     slidesPerView: 1,
+//     spaceBetween: 0,
+//     simulateTouch: true,
+//     centeredSlides: true,
+//     autoplay: 3500,
+//     speed: 1000,
+//     watchSlidesProgress: true,
+//     autoplayDisableOnInteraction: false
+// });
